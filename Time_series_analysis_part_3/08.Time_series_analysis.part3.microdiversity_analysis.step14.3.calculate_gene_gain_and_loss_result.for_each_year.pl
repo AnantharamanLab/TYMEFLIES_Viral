02559cdde1ff94970a0aa1aa9533233f700c748e
Zhichao Zhou <35715202+ChaoLab@users.noreply.github.com>
2023-11-03 23:19:17 -0500

    update
    update

--- Time_series_analysis_part_3/08.Time_series_analysis.part3.microdiversity_analysis.step14.3.calculate_gene_gain_and_loss_result.for_each_year.pl
+++ Time_series_analysis_part_3/08.Time_series_analysis.part3.microdiversity_analysis.step14.3.calculate_gene_gain_and_loss_result.for_each_year.pl
@@ -79,8 +79,60 @@ foreach my $viral_gene (sort keys %Viral_gene2gain_or_loss){
 }
 close OUT;
 
-# Step 4 Write down gene gain and loss result for four AMGs
-## Step 4.1 Store the viral species containing four AMGs
+# Step 4 Generate gene gain and loss result table for each viral genome and write it down
+## Step 4.1 Get all the viral genomes that have either gain or loss genes 
+my %Viral_gn_having_gain_or_loss_genes = (); # $viral_gn => the collection of $viral_genes (either gain or loss genes)
+foreach my $viral_gene (sort keys %Viral_gene2gain_or_loss){
+	my ($viral_gn_from_gene) = $viral_gene =~ /^(.+?)\_\_Ga/; 
+	if (!exists $Viral_gn_having_gain_or_loss_genes{$viral_gn_from_gene}){
+		$Viral_gn_having_gain_or_loss_genes{$viral_gn_from_gene} = $viral_gene;
+	}else{
+		$Viral_gn_having_gain_or_loss_genes{$viral_gn_from_gene} .= "\t".$viral_gene;
+	}
+}
+
+## Step 4.2 Generate the dict of Viral_gn2gain_or_loss_info
+my %Viral_gn2gain_or_loss_info = (); # $viral_gn => $num_of_gene_gain \t $num_of_gene_loss \t $genes_gain \t $genes_loss
+for my $viral_gn (sort keys %Viral_gn_having_gain_or_loss_genes){	
+	my $num_of_gene_gain = 0;
+	my $num_of_gene_loss = 0;
+	my $genes_gain = "";
+	my $genes_loss = "";
+	my @Genes_gain = ();
+	my @Genes_loss = ();	
+	
+	my @Viral_genes_either_gain_or_loss = split("\t", $Viral_gn_having_gain_or_loss_genes{$viral_gn});
+	
+	foreach my $viral_gene (sort @Viral_genes_either_gain_or_loss){
+		if ($Viral_gene2gain_or_loss{$viral_gene} eq "gain"){
+			$num_of_gene_gain++;
+			push @Genes_gain, $viral_gene;
+		}elsif($Viral_gene2gain_or_loss{$viral_gene} eq "loss"){
+			$num_of_gene_loss++;
+			push @Genes_loss, $viral_gene;
+		}	
+	}
+	
+	if (@Genes_gain){
+		$genes_gain = join("\,", @Genes_gain);
+	}
+
+	if (@Genes_loss){
+		$genes_loss = join("\,", @Genes_loss);
+	}
+
+	$Viral_gn2gain_or_loss_info{$viral_gn} = $num_of_gene_gain."\t".$num_of_gene_loss."\t".$genes_gain."\t".$genes_loss;
+}
+	
+## Step 4.3 Write down the Viral_gn2gain_or_loss_info.txt table
+open OUT, ">MetaPop.for_each_year/MetaPop/Viral_gn2gain_or_loss_info.txt";
+foreach my $viral_gn (sort keys %Viral_gn2gain_or_loss_info){
+	print OUT "$viral_gn\t$Viral_gn2gain_or_loss_info{$viral_gn}\n";
+}
+close OUT;
+
+# Step 5 Write down gene gain and loss result for four AMGs
+## Step 5.1 Store the viral species containing four AMGs
 my %Viral_species_containing_four_AMGs = (); # $viral_gn => $info (the corresponding information for each viral species gn)
 my @Viral_species_containing_four_AMGs = (); # Store the $viral_gn order
 open IN, "viral_species_containing_four_AMGs.txt";
@@ -95,7 +147,7 @@ while (<IN>){
 }
 close IN;
 
-## Step 4.2 Write down gene gain and loss result for four AMGs
+## Step 5.2 Write down gene gain and loss result for four AMGs
 open OUT, ">MetaPop.for_each_year/MetaPop/Viral_gene2gain_or_loss.for_four_AMGs.txt";
 print OUT "Viral gn\tViral gn characteristics\tNum of gene gain\tNum of gene loss\tGenes gained\tGenes lost\n";
 foreach my $viral_gn (@Viral_species_containing_four_AMGs){
